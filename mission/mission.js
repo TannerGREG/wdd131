2be7bb18867327b279a75d0b8b095111da4810c1
TannerGREG <bro24118@byui.edu>
2025-10-09 16:17:32 -0600

    corrected color scheme on mission page dark mode

--- mission/mission.js
+++ mission/mission.js
@@ -7,14 +7,14 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
-        document.body.style.backgroundColor = 'black';
+        document.body.style.backgroundColor = '#333';
         document.body.style.color = 'white';
         logo.src = 'byui-logo-white.png';
 
         localStorage.setItem('theme', 'dark');
     } else {
         document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
+        document.body.style.color = '#333';
         logo.src = 'byui-logo-blue.webp';
 
         localStorage.setItem('theme', 'light');
