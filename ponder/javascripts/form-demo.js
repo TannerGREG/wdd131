6faa92fd630b9f65858a26ae82b08b7da32cefae
TannerGREG <bro24118@byui.edu>
2025-10-30 20:58:44 -0600

    updated the forms page

--- ponder/javascripts/form-demo.js
+++ ponder/javascripts/form-demo.js
@@ -2,6 +2,7 @@
 function validateForm(event) {
   // get a reference to the form. Because we attached a submit event listener to the form itself, we can access the form either through 'event.target', or 'this'
   const theForm = event.target;
+  
   // the default behavior for a form submit is to try and navigate to another page where the form would be processed, if a url is not provided it will reload the current page. This sometimes is not desirable behavior. One case when we might do this is if we think there is bad data in the form.
   // To keep it from happening we can can call e.preventDefault()
   // You should always give feedback to the user about what whet wrong so they can fix it. We will store the error messages here
@@ -22,19 +23,28 @@ function validateForm(event) {
 }
 
 function togglePaymentDetails(e) {
+  console.log("change happened", e.target)
   // get a reference to the form. We can access all the named form inputs through the form element.
-  const theForm = ;
+  const theForm = document.querySelector("#payment");
   // we will also need the creditCardContainer and paypalUsernameContainer
-  const creditCardContainer = ;
-  const paypalContainer = ;
+  const creditCardContainer = document.querySelector("#creditCardInfo");
+  const paypalContainer = document.querySelector("#paypalInfo");
 
   // Hide payment containers by adding the '.hide' class to each of them
-
+  creditCardContainer.classList.add("hide");
+  paypalContainer.classList.add("hide");
   // Disable required for payment fields...if we hide a required field the browser will throw an error when we try to submit!
-
+  creditCardContainer.required = false;
+  paypalContainer.required = false;
 
   // Show the container based on the selected payment method, and add the required attribute back.
-
+  if(theForm.#payment.value === "Credit Card") {
+    creditCardContainer.classList.remove("hide");
+    theForm.creditCardContainer.required = true;
+  } else if (theForm.#payPalUsername.value === "Pay Pal") {
+      paypalContainer.classList.remove("hide");
+      theForm.paypalContainer.required = true;
+  }
 }
 
 // helper function to display our errors.
@@ -44,5 +54,6 @@ function showErrors(errors) {
   errorEl.innerHTML = html.join("");
 }
 // attach a change event handler to the paymentMethod input
-
+document.querySelector("#payment").addEventListener("change", togglePaymentDetails)
 // attach a submit event handler to the form
+document.querySelector("#checkoutForm"),addEventListener("submit", validateForm);
